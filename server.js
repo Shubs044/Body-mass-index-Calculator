623b6db8d3dd916d1815ecf043372a6a9189c679
Shubham Gautam <shubgautam044@gmail.com>
2023-06-05 00:42:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const bodyparser=require("body-parser");
 
 calcu.use(bodyparser.urlencoded({extended:true}));
 
-calcu.get("/", function(req,res){
-    res.sendFile(__dirname+"/index.html");
-});
+// calcu.get("/", function(req,res){
+//     res.sendFile(__dirname+"/index.html");
+// });
 
-calcu.post("/",function(req,res){
-    var num1=Number(req.body.num1);
-    var num2=Number(req.body.num2);
-    var result=num1+num2;
-    res.send("The result of the numbers is "+result);
-});
+// calcu.post("/",function(req,res){
+//     var num1=Number(req.body.num1);
+//     var num2=Number(req.body.num2);
+//     var result=num1+num2;
+//     res.send("The result of the numbers is "+result);
+// });
 
-calcu.get("/bmi",function(req,res){
+calcu.get("/",function(req,res){
     res.sendFile(__dirname+"/bmiCalculator.html");
 });
 
-calcu.post("/bmi",function(req,res){
+calcu.post("/",function(req,res){
     var h=Number(req.body.Height);
     var w=Number(req.body.Weight);
     var n=w/(h*h);
